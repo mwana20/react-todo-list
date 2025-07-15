a98618bdc31338f70d9c5967cb87919892025642
brooker <mubbala9@gmail.com>
2025-07-15 11:02:37 +0300

    w1

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,47 @@
+import { useState, useEffect } from "react";
+import "./styles.css";
+import { NewTodoForm } from "./NewTodoForm";
+import { TodoList } from "./TodoList"; // 
+
 export default function App() {
-  return <div>Hi</div>;
+  const [todos, setTodos] = useState(() =>{
+    const localValue = localStorage.getItem("ITEM");
+    if (localValue == null) return [];
+    
+    return JSON.parse(localValue);
+  });
+
+  useEffect(() => {
+    localStorage.setItem("ITEM", JSON.stringify(todos));
+
+  }, [todos])
+
+  function addTodo(title) {
+    setTodos(currentTodos => [
+      ...currentTodos,
+      { id: crypto.randomUUID(), title, completed: false }
+    ]);
+  }
+
+  function toggleTodo(id, completed) {
+    setTodos(currentTodos =>
+      currentTodos.map(todo =>
+        todo.id === id ? { ...todo, completed } : todo
+      )
+    );
+  }
+
+  function deleteTodo(id) {
+    setTodos(currentTodos =>
+      currentTodos.filter(todo => todo.id !== id)
+    );
+  }
+
+  return (
+    <>
+      <NewTodoForm onSubmit={addTodo} />
+      <h1 className="header">Todo List</h1>
+      <TodoList todos={todos} toggleTodo={toggleTodo} deleteTodo={deleteTodo} />
+    </>
+  );
 }
